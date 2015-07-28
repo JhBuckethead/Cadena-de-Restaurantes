16cf414a6ca6e9b5ef0c25f6ee647d09405e8a24
josecueva <jctacuri@gmail.com>
2015-07-27 19:34:34 -0500

    Base

--- Base/sico.sql
+++ Base/sico.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-07-2015 a las 23:12:10
+-- Tiempo de generación: 28-07-2015 a las 02:34:14
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -40,28 +40,12 @@ INSERT INTO `categoria` (`id_categoria`, `categoria`) VALUES
 (1, 'Pizzeria'),
 (2, 'Cafeteria'),
 (3, 'Restaurant'),
-(4, 'Eventos Sociales (alimentacion)'),
+(4, 'Eventos Sociales'),
 (5, 'Heladeria'),
 (6, 'Marisqueria'),
 (7, 'Licoreria'),
-(8, 'Comida Rapida'),
+(8, 'Comida rapida'),
 (9, 'Comida Vegetariana');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle`
---
-
-CREATE TABLE IF NOT EXISTS `detalle` (
-  `id_detalle` int(11) NOT NULL AUTO_INCREMENT,
-  `id_producto` int(11) NOT NULL,
-  `cantidad` int(11) DEFAULT NULL,
-  `id_factura` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_detalle`,`id_producto`),
-  KEY `fk_productos_idx` (`id_producto`),
-  KEY `fk_facturas_idx` (`id_factura`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -79,33 +63,65 @@ CREATE TABLE IF NOT EXISTS `empresa` (
   `estado` int(11) NOT NULL,
   PRIMARY KEY (`id_empresa`,`id_categoria`),
   KEY `fk_categoria_idx` (`id_categoria`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Volcado de datos para la tabla `empresa`
 --
 
 INSERT INTO `empresa` (`id_empresa`, `nombre`, `id_categoria`, `direccion`, `descripcion`, `url`, `estado`) VALUES
-(1, 'Pescaderia El chinito2', 2, 'Loja1', 'Comida del mar a su plato', 'http://static.amarillasinternet.com/pictures/200000_300000/280000_290000/281000_282000/281300_281400/281305/banners/281305_a.jpg', 1),
-(2, 'Topsy', 5, 'Loja', 'Helados en general.', 'http://www.loaizacomunicaciones.com.ec/_documentos/image/noticias/14/logo-topsy.png', 0),
-(3, 'jo2', 2, '2', '2dasda', '222d2', 1),
-(4, 'cccccklkkllklk', 5, 'aaaasssss', 'sssuouoo', 'sssslkllkll', 1),
-(5, 'jjjj', 2, 'jjdsadsa', 'j', 'jjjjj', 1);
+(1, 'Topsy2', 2, 'loja22', 'venta de helados2', 'http://www.loaizacomunicaciones.com.ec/_documentos/image/noticias/14/logo-topsy.png', 0),
+(2, 'El fogon2', 2, 'Parque Bolivar', 'Comida para el paso', 'http://www.surfguru.com/CMSImages/03/03f4ec44-1dee-4fe1-a91f-d2425eb30bba_390_339.jpg', 1),
+(3, 'NUEVA', 9, 'sac', 'cac', 'sdsad', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `facturas`
+-- Estructura de tabla para la tabla `pago`
 --
 
-CREATE TABLE IF NOT EXISTS `facturas` (
-  `id_factura` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `id_persona` int(11) NOT NULL,
-  PRIMARY KEY (`id_factura`),
-  KEY `fk_persona_idx` (`id_persona`),
-  KEY `persona_idx` (`id_persona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `pago` (
+  `id_pago` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `total_productos` int(20) DEFAULT NULL,
+  `total_pago` double DEFAULT NULL,
+  `numero_cuenta` int(20) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_pago`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `pago`
+--
+
+INSERT INTO `pago` (`id_pago`, `nombre`, `total_productos`, `total_pago`, `numero_cuenta`, `fecha`, `estado`) VALUES
+(1, 'jose', 8, 12, 1111111, '2015-07-27', 'PAGADO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `id_pedido` int(11) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `id_persona` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `fk_id_producto_idx` (`id_producto`),
+  KEY `fk_id_persona_idx` (`id_persona`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `pedido`
+--
+
+INSERT INTO `pedido` (`id_pedido`, `id_producto`, `cantidad`, `fecha`, `id_persona`) VALUES
+(1, 1, 5, '2015-07-27', 1),
+(2, 2, 3, '2015-07-27', 1);
 
 -- --------------------------------------------------------
 
@@ -122,7 +138,6 @@ CREATE TABLE IF NOT EXISTS `persona` (
   `fecha_nacimiento` date DEFAULT NULL,
   `telefono` int(11) DEFAULT NULL,
   `direccion` varchar(45) DEFAULT NULL,
-  `num_cuenta` varchar(45) DEFAULT NULL,
   `id_tipo` int(11) NOT NULL,
   `usuario` varchar(10) NOT NULL,
   `pass` varchar(10) NOT NULL,
@@ -134,8 +149,8 @@ CREATE TABLE IF NOT EXISTS `persona` (
 -- Volcado de datos para la tabla `persona`
 --
 
-INSERT INTO `persona` (`id_persona`, `nombres`, `mail`, `sexo`, `cedula`, `fecha_nacimiento`, `telefono`, `direccion`, `num_cuenta`, `id_tipo`, `usuario`, `pass`) VALUES
-(1, 'Pamela', 'pamela@gmail.com', 'Femenino', '1102235621', '2015-07-01', 98541236, 'Sauces', '115236524781', 1, 'pamela', 'pamela');
+INSERT INTO `persona` (`id_persona`, `nombres`, `mail`, `sexo`, `cedula`, `fecha_nacimiento`, `telefono`, `direccion`, `id_tipo`, `usuario`, `pass`) VALUES
+(1, 'jose', 'jjs@djj.com', 'Masculino', '31231', '2008-11-01', 4324, 'dasdsa', 2, 'jose', 'jose');
 
 -- --------------------------------------------------------
 
@@ -154,25 +169,17 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `estado` int(11) NOT NULL,
   PRIMARY KEY (`id_producto`,`id_empresa`),
   KEY `fk_empresa_idx` (`id_empresa`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
 INSERT INTO `productos` (`id_producto`, `nombre`, `descripcion`, `precio`, `id_empresa`, `stock`, `url`, `estado`) VALUES
-(1, 'ceviche', 'comida del mar', 50, 1, 20, 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Ceviche_mixto_890.JPG', 1),
-(2, 'sopa marinera33', 'sopa del mar', 53, 2, 50, 'http://www.photorecipestepbystep.com/wp-content/uploads/2012/10/sopa-marinera.jpg', 0),
-(3, 'Helado de coco', 'Gelatina con coco.', 0.75, 2, 30, 'http://4.bp.blogspot.com/-Gzcm8HgW5PM/U6sMRy1XDrI/AAAAAAAAACg/h_nHeS1A9Ic/s850/h2.jpg', 0),
-(4, 'Pastel de cumpleaños', 'De tamaño para 15 porciones', 10, 2, 100, 'http://img.fiesta101.com.s3.amazonaws.com/wp-content/uploads/2011/08/torta1.jpg', 0),
-(5, 'esfero', 'loja', 12, 2, 200, 'http://www.pceverest.com/imagenes/productos/esfero-espia-dorado.jpg', 0),
-(6, 'esfero', 'lojs', 10, 2, 100, 'http://www.pceverest.com/imagenes/productos/esfero-espia-dorado.jpg', 0),
-(7, 'ninguno', 'ninguno', 1, 1, 1, 'ninguno', 1),
-(8, 'al fin ', 'al fin ', 1, 2, 10, 'http://www.pceverest.com/imagenes/productos/esfero-espia-dorado.jpg', 0),
-(9, 'galo', 'galo', 1, 2, 1, 'lslsls', 0),
-(10, 'galo', 'galo', 1, 2, 1, 'galogalogalo', 0),
-(11, 'jose', 'jose', 1, 1, 1, 'dasd', 1),
-(12, 'pedro', 'pedro', 2, 1, 323, 'pedro', 1);
+(1, 'Helado de crema', 'con crema', 1.5, 2, 200, 'http://www.hayrecetas.com/wp-content/uploads/2008/12/helado-crema1.jpg', 1),
+(2, 'Parrilada', 'parrilla de chancho', 1.5, 2, 15, 'http://static.betazeta.com/www.sabrosia.com/up/2013/06/costillitas-al-horno-y-parrilla-960x623.jpg', 1),
+(3, 'Helado Yogurt', 'con yogurt', 0.5, 1, 20, 'http://s3-eu-west-1.amazonaws.com/mimandote/images/valoraciones/0001/3024/yogurt-helado.jpg?1333536282', 0),
+(4, 'no se', 'das', 1, 2, 21, 'ascasc', 0);
 
 -- --------------------------------------------------------
 
@@ -208,47 +215,6 @@ CREATE TABLE IF NOT EXISTS `valoraciones` (
   PRIMARY KEY (`id_valoraciones`,`id_persona`),
   KEY `fk_persona_idx` (`id_persona`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `detalle`
---
-ALTER TABLE `detalle`
-  ADD CONSTRAINT `fk_facturas` FOREIGN KEY (`id_factura`) REFERENCES `facturas` (`id_factura`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_productos` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `empresa`
---
-ALTER TABLE `empresa`
-  ADD CONSTRAINT `fk_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `facturas`
---
-ALTER TABLE `facturas`
-  ADD CONSTRAINT `persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `persona`
---
-ALTER TABLE `persona`
-  ADD CONSTRAINT `fk_tipo` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id_tipo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `fk_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `valoraciones`
---
-ALTER TABLE `valoraciones`
-  ADD CONSTRAINT `fk_persona` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
